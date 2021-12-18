31000962f5118472beb56c6f2aa3272159386a90
Sham-An <shamraev@inbox.ru>
2021-12-18 21:12:55 +0300

    Изучение PyQT (Python GUI) / Урок #2 – Библиотека PyQT5. Надписи и кнопки

---
+++ QT_tmp_GoshaDudar.py
@@ -0,0 +1,57 @@
+# https://www.youtube.com/playlist?list=PL0lO_mIqDDFXeDkOLHmEsL_HAEhw4-xDX
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Простая программа")
+        self.setGeometry(300, 250, 350, 200)
+
+        self.main_text = QtWidgets.QLabel(self)
+        # https://youtu.be/LtgsjK2VnJg?list=PL0lO_mIqDDFXeDkOLHmEsL_HAEhw4-xDX&t=552
+        self.main_text.setText("Это Базовая надпись")
+        self.main_text.move(100, 100)
+        # main_text.setFixedWidth(200)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText("Нажми на меня")
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_label)
+
+
+    def add_label(self):
+        print("add")
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = Window()
+    #
+    # window.setWindowTitle("Простая программа")
+    # window.setGeometry(300, 250, 350, 200)
+    #
+    # main_text = QtWidgets.QLabel(window)
+    # #https://youtu.be/LtgsjK2VnJg?list=PL0lO_mIqDDFXeDkOLHmEsL_HAEhw4-xDX&t=552
+    # main_text.setText("Это Базовая надпись")
+    # main_text.move(100, 100)
+    # #main_text.setFixedWidth(200)
+    # main_text.adjustSize()
+    #
+    # btn = QtWidgets.QPushButton(window)
+    # btn.move(70, 150)
+    # btn.setText("Нажми на меня")
+    # btn.setFixedWidth(200)
+    # btn.clicked.connect(add_label)
+    # #https://youtu.be/LtgsjK2VnJg?list=PL0lO_mIqDDFXeDkOLHmEsL_HAEhw4-xDX&t=840
+
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    application()
---
+++ README.md
@@ -0,0 +1,12 @@
+# pyqt5_camera_viewer
+#https://www.youtube.com/watch?v=wSv4-Ne8e-Q
+#https://github.com/berrouba-med-amine/simple-python-camera-viewer-opencv3-PyQt5
+In this example, we demonstrate how to create simple camera viewer using Opencv3 and PyQt5.
+
+If you  need to change ui_main_window.ui with QtDesigner, don't forget to regenerate ui_main_window.py file using this command:  
+```{r, engine='sh', count_lines}
+pyuic5 main_window.ui -o main_window.py
+```
+
+
+
